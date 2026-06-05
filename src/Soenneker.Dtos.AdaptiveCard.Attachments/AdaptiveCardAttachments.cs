fc82707a388a3ad4963c3b05a59b447857a52e30
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:36:22 -0500

    xml commenting

--- src/Soenneker.Dtos.AdaptiveCard.Attachments/AdaptiveCardAttachments.cs
+++ src/Soenneker.Dtos.AdaptiveCard.Attachments/AdaptiveCardAttachments.cs
@@ -8,14 +8,23 @@ namespace Soenneker.Dtos.AdaptiveCard.Attachments;
 /// </summary>
 public class AdaptiveCardAttachments
 {
+    /// <summary>
+    /// Gets or sets content type.
+    /// </summary>
     [JsonProperty("contentType")]
     [JsonPropertyName("contentType")]
     public string ContentType { get; set; }
 
+    /// <summary>
+    /// Gets or sets content url.
+    /// </summary>
     [JsonProperty("contentUrl")]
     [JsonPropertyName("contentUrl")]
     public string? ContentUrl { get; set; }
 
+    /// <summary>
+    /// Gets or sets content.
+    /// </summary>
     [JsonProperty("content")]
     [JsonPropertyName("content")]
     public AdaptiveCards.AdaptiveCard? Content { get; set; }
